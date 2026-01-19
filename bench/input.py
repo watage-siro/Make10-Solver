fdcbfab01711e9d0f8dc33b63246765c01204f9c
watage_siro <chisa_0708@icloud.com>
2026-01-19 20:33:34 +0900

    Update input.py

--- bench/input.py
+++ bench/input.py
@@ -4,11 +4,11 @@ from pathlib import Path
 Path("bench/inputs").mkdir(parents=True, exist_ok=True)
 
 n = 6
+data_siza = 1
 
-for n in range(2, 9):
-    for _ in range(10):
-        nums = [random.randint(1, 9) for _ in range(n)]
-        filename = f"{n}({','.join(map(str, nums))}).txt"
-        with open(f"bench/inputs/{filename}", "w") as f:
-            f.write(str(n) + "\n")
-            f.write(" ".join(map(str, nums)))
+for _ in range(data_size):
+    nums = [random.randint(1, 9) for _ in range(n)]
+    filename = f"{n}({','.join(map(str, nums))}).txt"
+    with open(f"bench/inputs/{filename}", "w") as f:
+        f.write(str(n) + "\n")
+        f.write(" ".join(map(str, nums)))

e32e0ad67b31cd3252df42cdc205e24fa3a8f119
watage_siro <chisa_0708@icloud.com>
2026-01-19 18:36:56 +0900

    Update input.py

--- bench/input.py
+++ bench/input.py
@@ -1,1 +1,9 @@
+import random, os
 
+random.seed(114514)
+os.makedirs("bench/inputs", exist_ok=True)
+
+for idx in range(1, 11):
+    nums = [random.randint(1, 9) for _ in range(6)]
+    with open(f"bench/inputs/input{idx}.txt", "w") as f:
+        f.write(" ".join(map(str, nums)))

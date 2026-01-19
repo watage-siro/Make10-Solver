bdbad95454ffa5265a328f678dc356da4974e92c
watage_siro <chisa_0708@icloud.com>
2026-01-19 19:47:42 +0900

    Update compute.py

--- bench/compute.py
+++ bench/compute.py
@@ -1,18 +1,17 @@
 import statistics, sys
 
-times = list(map(float, sys.argv[1::4]))
 calls = list(map(int, sys.argv[1::4]))
 ops = list(map(int, sys.argv[2::4]))
 evals = list(map(int, sys.argv[3::4]))
+times = list(map(float, sys.argv[4::4]))
 
-
-print(",".join(map(str, times + calls + ops + evals + [
-    statistics.mean(times),
+print(",".join(map(str, calls + ops + evals + times + [
     statistics.mean(calls),
     statistics.mean(ops),
     statistics.mean(evals),
-    min(times),
-    max(times),
+    statistics.mean(times),
     min(calls),
     max(calls),
+    min(times),
+    max(times),
 ])))
